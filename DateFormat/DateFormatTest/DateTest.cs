23e16d877d8582d9fc49bfd71216f44c1bfdf4db
Julian Graf <graf_julian@icloud.com>
2025-09-29 14:03:32 +0200

    DateTest.cs AreNotEqual-Funktion hinzugefügt

--- DateFormat/DateFormatTest/DateTest.cs
+++ DateFormat/DateFormatTest/DateTest.cs
@@ -55,5 +55,56 @@ namespace DateFormatTest
             // Assert
             Assert.AreEqual(expectedResult, result);
         }
+
+        [TestMethod]
+        public void TestDateFormatWrong_CH()
+        {
+            // Arrange
+            int tag = 18;
+            int month = 11;
+            int year = 2008;
+            string chooseFormat = "CH";
+            string expectedResult = "2008-11-18";
+
+            // Act
+            string result = FormatDate.FormatDatum(tag, month, year, chooseFormat);
+
+            // Assert
+            Assert.AreNotEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestDateFormatWrong_US()
+        {
+            // Arrange
+            int tag = 18;
+            int month = 11;
+            int year = 2008;
+            string chooseFormat = "US";
+            string expectedResult = "18/11/2008";
+
+            // Act
+            string result = FormatDate.FormatDatum(tag, month, year, chooseFormat);
+
+            // Assert
+            Assert.AreNotEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestDateFormatWrong_ISO()
+        {
+            // Arrange
+            int tag = 18;
+            int month = 11;
+            int year = 2008;
+            string chooseFormat = "ISO";
+            string expectedResult = "18.11.2008";
+
+            // Act
+            string result = FormatDate.FormatDatum(tag, month, year, chooseFormat);
+
+            // Assert
+            Assert.AreNotEqual(expectedResult, result);
+        }
     }
 }
