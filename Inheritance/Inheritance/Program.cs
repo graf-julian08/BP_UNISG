2f5b9009821443d23468698923afd387d72db5b0
Julian Graf <graf_julian@icloud.com>
2025-09-30 10:43:36 +0200

    Abstract Method Anpassung

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -6,26 +6,31 @@ namespace InheritanceCars
     {
         void WhatAreYou();
     }
+
     public interface GermanCar
     {
         void WhatAreYou();
     }
+
     public abstract class Car
     {
         public virtual void WhatAreYou() { Console.WriteLine("I am a Car"); }
     }
+
     public abstract class Golf : GermanCar
     {
         public virtual void WhatAreYou() { Console.WriteLine("I am a Golf"); }
     }
+
     public class GolfGTI : Golf, SportsCar
     {
         public override void WhatAreYou() { Console.WriteLine("I am a Golf GTI"); }
     }
     public class Manta : Opel, SportsCar
     {
-        public override void WhatAreYou() { Console.WriteLine("eeh du, echt cool"); }
+        public new void WhatAreYou() { Console.WriteLine("eeh du, echt cool"); }
     }
+
     public class Opel : Car, GermanCar
     {
         public override void WhatAreYou() { Console.WriteLine("I am a Opel"); }
@@ -39,12 +44,12 @@ namespace InheritanceCars
             Car car1 = new Manta();
             Golf car2 = new GolfGTI();
 
-            ((Opel)car1).WhatAreYou(); // output ?
-            ((GolfGTI)car2).WhatAreYou();
-            ((Manta)car1).WhatAreYou();
-            ((SportsCar)car1).WhatAreYou();
-            car2.WhatAreYou();
-            car1.WhatAreYou();
+            ((Opel)car1).WhatAreYou(); // output = I'm a Opel
+            ((GolfGTI)car2).WhatAreYou(); // I'm a GTI
+            ((Manta)car1).WhatAreYou(); // ehh du, echt cool
+            ((SportsCar)car1).WhatAreYou(); // eeh du, echt cool
+            car2.WhatAreYou(); // I'm a Golf GTI
+            car1.WhatAreYou(); // I'm a Opel
         }
     }
 }
